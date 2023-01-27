21be5147148661aab3e743dd64683e9aefea8ea7
Wilton <wiltoncamilol@gmail.com>
2023-01-27 15:41:51 -0500

    resubmit

--- index.js
+++ index.js
@@ -1,81 +1,116 @@
-const baseURL=('https://wttr.in')
-const form = document.querySelector("form")
-const userSearch = document.querySelector(".userSearch")
+const BASE_URL = `https://wttr.in`
 
-const currentWeather = document.querySelector('current-weather')
-const todayWeather = document.querySelector('#today-weather')
-const tomorrowWeather = document.querySelector('#tomorrow-weather')
-const thirdDayWeather = document.querySelector('#third-day-weather')
+const searchResult = document.querySelector(".searchResults")
+const form = document.querySelector("form");
+const currentWeatherArticle = document.querySelector("#current-weather")
+const todayWeatherArticle = document.querySelector("#today")
+const tomorrowWeatherArticle = document.querySelector("#tomorrow")
+const dayAfterWeatherArticle = document.querySelector("#day-after")
 const main = document.querySelector("main")
 const ul = document.querySelector("#search-ul")
 const p = document.querySelector(".chooseLocation")
+let formattedUrl
+form.addEventListener("submit", getCurrentApi)
 
-form.addEventListener("submit", getApiData)
-
-function getApiData(event){
-    event.prventdefault()
-const location = `${userSearch.value}`
-const urlFormat = `${BASE_URL} ${location}?format=j1`
-}
-
-function getWeather(url, location){
-    fetch(url)
-        .then((response) => response.json())
-        .then(result => {
-    createCurrentWeather(result,location)        
-        })
-        .catch((error) => {
-            createErrorMessage(error)
-            function createErrorMessage(){}
-
-        })
-}       
-function createCurrentWeather(result,location){
-    appendCurrentWeather(result,location)
-    getTodayWeather(result,location)
-    getTomorrowWeather(result,location)
-    getThirdDayWeather(result,location)
-    getPreviousSearches(result,location)
+function getCurrentApi(event) {
+    event.preventDefault();
+    const location = `${searchResult.value}`
+     formattedUrl = `${BASE_URL}/${location}?format=j1`
+    getWeatherAppData(formattedUrl, location)
     form.reset()
 }
 
-function appendCurrentWeather(result,location){
-    const h1 = document.createElement(h1)
-    h1.textContent = location
-    const area = document.createElement('p')
-    area.innerHTML = `<strong>Area:</strong> ${result.nearest_area[0].value}`
+function getWeatherAppData(url, location) {
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+            
+            console.log("I'm in Fetch")
+            createCurrentWeather(result, location);
+            
+        })
+        .catch((error) => {
+            
+           createErrorMessage(error)
+            
+           function createErrorMessage(){}
+        });
+}
+function createCurrentWeather(result, location) {
+    console.log(result, location)
+    appendCurrentWeatherResult(result, location)
+    getTodayWeatherArticle(result, location)
+    getTomorrowWeatherArticle(result, location)
+    getDayAfterWeatherArticle(result, location)
+    getPreviousSearches(result, location)
+}
+function appendCurrentWeatherResult(result, location) {
+    currentWeatherArticle.textContent = ""
+    const h2 = document.createElement('h2');
+    h2.textContent = location
+    const area = document.createElement("p");
+    if (`${result.nearest_area[0].areaName[0].value}` === location){
+        area.innerHTML = `<strong>Area:</strong> ${result.nearest_area[0].areaName[0].value}`
+    } else {
+        area.innerHTML = `<strong>Nearest Area:</strong> ${result.nearest_area[0].areaName[0].value}`
+    } 
+    
     const region = document.createElement("p")
-    region.innerHTML = `<strong>Region</strong> ${result.nearest_region[0].value}`
+    region.innerHTML = `<strong>Region:</strong> ${result.nearest_area[0].region[0].value} `
     const country = document.createElement("p")
-    country.innerHTML = `<strong>Country</strong> ${result.nearest_country[0].value}`
+    country.innerHTML = `<strong>Country:</strong> ${result.nearest_area[0].country[0].value}`
     const current = document.createElement("p")
-    current.innerHTML = `<strong>Country</strong> ${result.nearest_country[0].value}`
-    currentWeather.append(h1, area, region, country, current)
-}   
-
-function getTodayWeather(result){
+    current.innerHTML = `<strong>Current:</strong> Feels Like ${result.current_condition[0].FeelsLikeF}°F`
+    currentWeatherArticle.append(h2, area, region, country, current)
+    
+}
+function getTodayWeatherArticle(result) {
+    todayWeatherArticle.textContent = ""
     const today = document.createElement('p');
-    today.innerHTML = `<strong> Today </strong>
-     <p> <strong> Average Temp : </strong> ${result.weather[0].avgtempF} F°</p>
-     <p> <strong> Max Temp : </strong> ${result.weather[0].maxtempF} F°</p>
-     <p> <strong> Min Temp : </strong>${result.weather[0].mintempF} F°</p>`
-    todayWeather.append(today)
+    today.innerHTML = `<strong>Today</strong> 
+    <p> <strong> Average Temperature: </strong> ${result.weather[0].avgtempF}°F</p> 
+    <p> <strong> Max Temperature: </strong> ${result.weather[0].maxtempF}°F </p>
+    <p> <strong> Min Temperature: </strong> ${result.weather[0].mintempF}°F </p>`
+    todayWeatherArticle.append(today) 
 }
 
-function getTodayWeather(result){
-    const tomorrowWeather = document.createElement('p');
-    today.innerHTML = `<strong> Today </strong>
-     <p> <strong> Average Temp : </strong> ${result.weather[0].avgtempF} F°</p>
-     <p> <strong> Max Temp : </strong> ${result.weather[0].maxtempF} F°</p>
-     <p> <strong> Min Temp : </strong>${result.weather[0].mintempF} F°</p>`
-    todayWeather.append(tomorrowWeather)
+function getTomorrowWeatherArticle(result) {
+    tomorrowWeatherArticle.textContent = " "
+    const tomorrow = document.createElement("p")
+    tomorrow.innerHTML = `<strong>Tomorrow</strong> 
+    <p> <strong> Average Temperature: </strong> ${result.weather[1].avgtempF}°F</p> 
+    <p> <strong> Max Temperature: </strong> ${result.weather[1].maxtempF}°F </p>
+    <p> <strong> Min Temperature: </strong> ${result.weather[1].mintempF}°F </p>`
+    tomorrowWeatherArticle.append(tomorrow)
 }
 
-function getTodayWeather(result){
-    const thirdDay = document.createElement('p');
-    today.innerHTML = `<strong> Today </strong>
-     <p> <strong> Average Temp : </strong> ${result.weather[0].avgtempF} F°</p>
-     <p> <strong> Max Temp : </strong> ${result.weather[0].maxtempF} F°</p>
-     <p> <strong> Min Temp : </strong>${result.weather[0].mintempF} F°</p>`
-    todayWeather.append(thirdDay)
+function getDayAfterWeatherArticle(result) {
+    dayAfterWeatherArticle.textContent = " "
+    const dayAfter = document.createElement("p")
+    dayAfter.innerHTML = `<strong>Day After Tomorrow</strong>
+    <p> <strong> Average Temperature: </strong> ${result.weather[2].avgtempF}°F</p> 
+    <p> <strong> Max Temperature: </strong> ${result.weather[2].maxtempF}°F </p>
+    <p> <strong> Min Temperature: </strong> ${result.weather[2].mintempF}°F </p>`
+    dayAfterWeatherArticle.append(dayAfter)
+   
 }
+
+function getPreviousSearches(result, location) {
+    console.log("test")
+    const li = document.createElement("li")
+    const a = document.createElement("a")
+    a.setAttribute("href", "#")
+    a.setAttribute("name", formattedUrl)
+    a.textContent = location 
+    const pElement = document.createElement("p")
+    pElement.textContent = `- ${result.current_condition[0].FeelsLikeF}°F`
+     ul.append(li)
+    li.append(a)
+a.after(pElement)
+const search = document.querySelector("#no-previous-search")
+    search.remove()
+    a.addEventListener("click", (event)=>{
+        getWeatherAppData(`${event.target.name}`, location)
+    })
+
+};
